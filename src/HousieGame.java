2d550f7fb0af9a7742bca15bad85f02cdb12ee34
prajwal244 <83490407+prajwal244@users.noreply.github.com>
2025-06-21 17:19:14 +0530

    Update HousieGame.java

--- src/HousieGame.java
+++ src/HousieGame.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.IntStream;
 
-public class HousieGame {
+public class HousieGame {//class
 
 	public static void main(String[] args) {
 		IntStream.range(0, 5).forEach(value -> {
